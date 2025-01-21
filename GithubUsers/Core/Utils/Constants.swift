848dcd128d8e42912e8c615093cb2b1b3f86b159
Tuan <hoangtuanfithou@gmail.com>
2025-01-21 23:30:15 +0900

    Update the token, as it has been automatically revoked

--- GithubUsers/Core/Utils/Constants.swift
+++ GithubUsers/Core/Utils/Constants.swift
@@ -11,8 +11,8 @@ struct Constants {
     static let baseURL = "https://api.github.com"
 
     /// The API authentication token.
-    /// **Note:** Replace this with a secure token in production.
-    static let token = "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"
+    /// **Note:** Replace with github token https://github.com/settings/tokens or keep empty with limited access.
+    static let token = ""
 
     /// The number of items to fetch per page.
     static let pageSize = 20
